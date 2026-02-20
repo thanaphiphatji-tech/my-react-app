a5aa0cb1c6f0e784f1f2e7430217401ef3724962
thanaphiphatji-tech <thanaphiphat.ji@gmail.com>
2026-02-20 22:13:33 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-react-app/', 
+  base: '/my-react-app/',   // 👈 สำคัญมากสำหรับ GitHub Pages
+
+  server: {
+    port: 5173,
+    proxy: {
+      '/gas': {
+        target: 'https://script.google.com',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) =>
+          path.replace(
+            /^\/gas/,
+            '/macros/s/AKfycby1YzD0g7VJq-NPMfulSu6re6Ikwh7HeU3uwl_OAIWXCadHvtAzT8oork-KJbwCdFvKmA/exec'
+          )
+      }
+    }
+  }
 })
